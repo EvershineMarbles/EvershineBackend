e5b389b6629134e75cd400e3843a29448f99d1cb
Sonal Baheti <sonal.21mcan176@jecrcu.edu.in>
2025-02-27 08:22:33 +0530

    Initial backend setup with secure configuration

--- src/database/dbConnection.js
+++ src/database/dbConnection.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 function dbConnect() {
   mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
+    .connect("mongodb+srv://osomeblosome:sTrQNC1gFqNL9BCE@cluster0.0omkg.mongodb.net/evershine?retryWrites=true&w=majority&appName=Cluster0", {
+      useNewUrlParser: true,
       useUnifiedTopology: true,
     })
     .then(() => console.log("MongoDB Connected"))
