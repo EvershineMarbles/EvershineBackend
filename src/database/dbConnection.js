583087b09df6d3fc909e0b419b115c37b351bc89
Sonal Baheti <sonal.21mcan176@jecrcu.edu.in>
2025-02-27 11:13:04 +0530

    Update

--- src/database/dbConnection.js
+++ src/database/dbConnection.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 function dbConnect() {
   mongoose
-    .connect("mongodb+srv://laveshcharmteam:jT1zEfVfIaWhgOCs@cluster0.axazp.mongodb.net/product", {
-      useNewUrlParser: true,
+  .connect("mongodb+srv://osomeblosome:sTrQNC1gFqNL9BCE@cluster0.0omkg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
+    useNewUrlParser: true,
       useUnifiedTopology: true,
     })
     .then(() => console.log("MongoDB Connected"))
